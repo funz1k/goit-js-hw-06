e88695172eaa51b9af6a804c7312182a7db865d4
funz1k <92177445+funz1k@users.noreply.github.com>
2022-02-23 15:50:08 +0200

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+
+const ingredientsEl = options =>  {
+  return options.map(element => {
+  const listEl = document.createElement('li')
+  listEl.textContent = element
+  listEl.classList.add('item')
+  return listEl
+})};
+
+ingredientsList.append(...ingredientsEl(ingredients))
+
+
