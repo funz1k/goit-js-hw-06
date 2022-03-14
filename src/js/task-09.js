1ba410366c420bf1b60ad3922e8d14a500da5d08
funz1k <92177445+funz1k@users.noreply.github.com>
2022-03-14 15:33:01 +0200

    tsak - 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
+const refs = {
+  colorName: document.querySelector('.color'),
+  changeColorBtn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+}
+
+const onChangeTargetClick = (event) => {
+  refs.colorName.textContent = getRandomHexColor();
+  changeBodyColor();
+};
+
+const changeBodyColor = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+refs.changeColorBtn.addEventListener('click', onChangeTargetClick);
