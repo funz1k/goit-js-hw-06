72c3cafd489adef2315e9b05ed801fc192d61eb1
funz1k <92177445+funz1k@users.noreply.github.com>
2022-03-14 14:57:53 +0200

    task - 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,31 @@
+const refs = {
+    loginForm: document.querySelector('.login-form'),
+    submitBtn: document.querySelector('button'),
+}
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formElement = event.currentTarget.elements;
+    
+    const email = formElement.email.value;
+    const password = formElement.password.value;
+
+    if (email === '' || password === '') {
+        alert('Все поля формы должны бать заполнены')
+    } else {
+        const formData = {
+            email, password
+        }
+
+        console.log(formData);
+        formReset();
+    }
+
+}
+
+const formReset = () => {
+    refs.loginForm.reset()
+}
+
+refs.loginForm.addEventListener('submit', onFormSubmit);
